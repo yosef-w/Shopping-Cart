272650767586617181cb211cee8f6e06c6ab6588
yosef-w <ywolday24@gmail.com>
2023-02-26 11:43:13 -0600

    added a while loop to 'add_items'

--- shoppingcart.py
+++ shoppingcart.py
@@ -1,5 +1,6 @@
 def shopping_cart():
     responses = {}
+    total = 0
     active = True
     while active:
         name = input('Welcome to Yosef\'s store! What is your name? ')
@@ -11,34 +12,50 @@ def shopping_cart():
             if add_items:
                 price_items = float(input(f'Looks like you chose to add {add_items}. Please enter the price: $'))
                 if price_items:
-                    # add item to dictionary
+                    responses[add_items] = price_items
                     print(f'You have successfully added {add_items} to your shopping cart!')
-                    retry = input('Would you like to add another item?')
-                    if retry.title() == 'No' or retry.title() == 'N':
-                        active = False
-                responses[add_items] = price_items
+                    while True:
+                        retry = input('Would you like to add another item? ')
+                        if retry.title() == 'Yes' or retry.title() == 'Y':
+                            add_items = input('What would you like to add? ')
+                            if add_items:
+                                price_items = float(input(f'Looks like you chose to add {add_items}. Please enter the price: $'))
+                                if price_items:
+                                    responses[add_items] = price_items
+                                    print(f'You have successfully added {add_items} to your shopping cart!')
+                        elif retry.title() == 'No' or retry.title() == 'N':
+                            break
+                        else:
+                            print('That is not a correct ansnwer. Please input "Yes" or No"')
+
 
 
         elif opening.title() == 'Delete' or 'Delete' in opening.title():
-            delete_items = input(f'Are you sure you would like to remove {add_items} from your shopping cart? ')
-            if delete_items:
-                # delete item from dictionary
-                del responses[add_items]
+            delete_items = input(f'What item would you like to remove? ')
+            if delete_items.title():
+                del responses[delete_items]
                 print(f'You have successfully removed {delete_items} to your shopping cart!')
                 
 
         elif opening.title() == 'View Shopping Cart' or 'View' in opening.title():
-            # show final shopping cart
-            print('----------Shopping Cart----------')
+            print('\n----------Shopping Cart----------')
             for key, value in responses.items():
-                print (f"\t{key.title()}   x   {value}")
+                total += value
+                print (f"\t{key.title()}   x   ${value}")
             print('--------------Total--------------')
+            print(f'Your total for today is: {total}\n')
+
         elif opening.title() == 'Quit':
             active = False 
 
 
+
+    print(f'\n------{name.title()}\'s Shopping Cart------')
     for key, value in responses.items():
-        print (f"{key.title()}'s address is {value}.")
+        print (f"\t{key.title()}   x   ${value}")
+    print('--------------Total--------------')
+    print(f'Your total for today is: {total}\n')
+            
 
 
 shopping_cart()
